721cbf3151e1228516c905b1afe5e353fac47bbd
Takee Dev <peerapolkoeram@gmail.com>
2026-02-18 18:57:36 +0700

    init project

---
+++ src/main/java/takee/dev/gpg/GpgApplication.java
@@ -0,0 +1,13 @@
+package takee.dev.gpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/takee/dev/gpg/GpgApplicationTests.java
@@ -0,0 +1,13 @@
+package takee.dev.gpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
